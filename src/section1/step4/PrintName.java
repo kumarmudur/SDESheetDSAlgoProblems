931c6f1ca6e5cf495b965c59a26ea365f3af5af0
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-09-21 08:16:31 +0530

    [JAVA][SDE Sheet][Section 1][Step 4] print names n times example solution

---
+++ src/section1/step4/PrintName.java
@@ -0,0 +1,29 @@
+package section1.step4;
+
+public class PrintName {
+
+    // Solution 1: Iterative
+    static void printNames(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println("shiva");
+        }
+    }
+
+    // Solution 2: Recursion
+    static void printNames1(int n) {
+        if (n == 0) return;
+        System.out.println("shiva");
+        printNames1(n - 1);
+    }
+
+    // Solution 3: Recursion
+    static void printNames2(int n, int i) {
+        if (i > n) return;
+        System.out.println("shiva");
+        printNames2(n, i + 1);
+    }
+
+    public static void main(String[] args) {
+        printNames1(5);
+    }
+}
