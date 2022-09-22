1af5af645565a65efb3fa600c7b5f719b69c6753
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-09-22 22:06:09 +0530

    [JAVA][SDE Sheet][Section 1][Step 4] factorial of number example solution

--- src/section1/step4/Factorial.java
+++ src/section1/step4/Factorial.java
@@ -2,7 +2,7 @@ package section1.step4;
 
 public class Factorial {
 
-    // time: O(N) | space: O(1)
+    // time: O(N) | space: O(N)
     static int factorial(int n) {
         if (n < 1) return 1;
         return n * factorial(n - 1);
