d5f9193710b32db20530a437e7f61beb216b11bb
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-09-24 10:03:34 +0530

    [JAVA][SDE Sheet][Section 1][Step 4] reverse array using recursion example solution

--- src/section1/step4/ReverseArray.java
+++ src/section1/step4/ReverseArray.java
@@ -9,17 +9,38 @@ public class ReverseArray {
         int left = 0;
         int right = arr.length - 1;
         while (left < right) {
-            int temp = arr[left];
-            arr[left] = arr[right];
-            arr[right] = temp;
+            swap(arr, left, right);
             left++;
             right--;
         }
         return arr;
     }
 
+    static int[] reverse1(int[] arr, int left, int right) {
+      if (left >= right) return arr;
+      swap(arr, left, right);
+      reverse1(arr, left + 1, right - 1);
+      return arr;
+    };
+
+    static void swap(int[] arr, int left, int right) {
+        int temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+    }
+
+    static int[] reverse2(int[] arr, int index) {
+        int n = arr.length;
+        if (index >= n / 2) return arr;
+        swap(arr, index, n - index - 1);
+        reverse2(arr, index + 1);
+        return arr;
+    }
+
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4, 5};
-        System.out.println(Arrays.toString(reverse(arr)));
+//      System.out.println(Arrays.toString(reverse(arr)));
+
+        System.out.println(Arrays.toString(reverse2(arr, 0)));
     }
 }
