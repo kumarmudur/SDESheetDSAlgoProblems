5f3044d869734b9194868f01d6cb45e9b39aa6ad
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-09-25 17:18:23 +0530

    [JAVA][SDE Sheet][Section 3][Step 1][Easy][Arrays][Two Pointers] remove duplicates example solution 2

--- src/section3/step1_easy/RemoveDuplicate.java
+++ src/section3/step1_easy/RemoveDuplicate.java
@@ -2,6 +2,7 @@ package section3.step1_easy;
 
 // Topic: Arrays, Two Pointers, HashSet
 
+import java.util.Arrays;
 import java.util.HashSet;
 
 public class RemoveDuplicate {
@@ -21,9 +22,21 @@ public class RemoveDuplicate {
         return k;
     }
 
+    // time: O(N) | space: O(1)
+    static int removeDuplicates1(int[] arr) {
+        int i = 0;
+        for (int j = 1; j < arr.length; j++) {
+            if (arr[i] != arr[j]) {
+                i++;
+                arr[i] = arr[j];
+            }
+        }
+        return i + 1;
+    }
+
     public static void main(String[] args) {
         int[] arr = { 1,1,2,2,2,3,3 };
-        int k = removeDuplicates(arr);
+        int k = removeDuplicates1(arr);
         System.out.println("The array after removing duplicate elements is: ");
         for (int i = 0; i < k; i++) {
             System.out.print(arr[i] + " ");
