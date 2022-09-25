7c49799fec83a815ac6381cef1d928e30adeb348
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-09-25 18:40:40 +0530

    [JAVA][SDE Sheet][Section 3][Step 1][Easy][Arrays] move all zeros to end example solution 2

--- src/section3/step1_easy/MoveAllZerosToEnd.java
+++ src/section3/step1_easy/MoveAllZerosToEnd.java
@@ -22,8 +22,34 @@ public class MoveAllZerosToEnd {
         System.out.println("temp " + Arrays.toString(temp));
     }
 
+    // time: O(N) | space: O(1)
+    static void moveAllZerosToEnd1(int[] arr) {
+        int k = 0;
+        int length = arr.length;
+        while (k < arr.length) {
+            if (arr[k] == 0) break;
+            else k = k + 1;
+        }
+
+        int i = k; int j = k + 1;
+
+        while (i < length && j < length) {
+            if (arr[j] != 0) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+            }
+            j++;
+        }
+
+        for (int element: arr) {
+            System.out.print(element + " ");
+        }
+    }
+
     public static void main(String[] args) {
-        int[] arr = { 1, 0, 2, 3, 0, 4, 0, 1 };
-        moveAllZerosToEnd(arr);
+        int[] arr = { 0, 0, 1, 0, 2, 3, 0, 4, 0, 1 };
+        moveAllZerosToEnd1(arr);
     }
 }
