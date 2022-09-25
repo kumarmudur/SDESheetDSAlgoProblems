95fd1946bbaf8f3b71dff0114729472fa9f09e61
Basavakumar Mudur <kumarmudur10@gmail.com>
2022-09-25 17:38:03 +0530

    [JAVA][SDE Sheet][Section 3][Step 1][Easy][Arrays] rotate array by one position example solution 2

--- src/section3/step1_easy/RotateArrayByOne.java
+++ src/section3/step1_easy/RotateArrayByOne.java
@@ -12,14 +12,25 @@ public class RotateArrayByOne {
             temp[i - 1] = arr[i];
         }
         temp[n - 1] = arr[0];
-        for (int i = 0; i < n; i++) {
-            System.out.print(temp[i] + " ");
+        for (int index : arr) {
+            System.out.print(index + " ");
+        }
+    }
+
+    static void rotateArrayByOne1(int[] arr, int n) {
+        int temp = arr[0];
+        for (int i = 0; i < n - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+        arr[n - 1] = temp;
+        for (int index : arr) {
+            System.out.print(index + " ");
         }
     }
 
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4, 5};
         int n = 5;
-        rotateArrayByOne(arr, n);
+        rotateArrayByOne1(arr, n);
     }
 }
